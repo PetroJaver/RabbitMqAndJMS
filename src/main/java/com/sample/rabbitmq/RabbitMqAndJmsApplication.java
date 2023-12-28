98ac34b480033db3e8af5bf5e1d3463b985e09d8
Peter S <peter.s@implemica.com>
2023-12-28 15:50:04 +0200

    Fix receiving messages

--- src/main/java/com/sample/rabbitmq/RabbitMqAndJmsApplication.java
+++ src/main/java/com/sample/rabbitmq/RabbitMqAndJmsApplication.java
@@ -4,7 +4,13 @@ import com.rabbitmq.jms.admin.RMQConnectionFactory;
 import jakarta.jms.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -17,5 +23,13 @@ public class RabbitMqAndJmsApplication {
     @Bean
     ConnectionFactory connectionFactory() {
         return new RMQConnectionFactory();
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        return factory;
     }
 }
--- src/main/java/com/sample/rabbitmq/StockConsumer.java
+++ src/main/java/com/sample/rabbitmq/StockConsumer.java
@@ -5,30 +5,17 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import org.springframework.jms.listener.adapter.MessageListenerAdapter;
+import org.springframework.util.ErrorHandler;
 
 @Configuration
 public class StockConsumer {
     private static final Log log = LogFactory.getLog(StockConsumer.class);
 
-    @Bean
-    DefaultMessageListenerContainer jmsListener(ConnectionFactory connectionFactory) {
-        DefaultMessageListenerContainer jmsListener = new DefaultMessageListenerContainer();
-        jmsListener.setConnectionFactory(connectionFactory);
-        jmsListener.setDestinationName("rabbit-trader-channel");
-        jmsListener.setPubSubDomain(true);
-
-        MessageListenerAdapter adapter = new MessageListenerAdapter(new Receiver());
-        adapter.setDefaultListenerMethod("receive");
-        jmsListener.setMessageListener(adapter);
-
-        return jmsListener;
-    }
-
-    protected static class Receiver {
-        public void receive(String message) {
-            log.info("Received " + message);
-        }
+    @JmsListener(destination = "rabbit-trader-channel", containerFactory = "myFactory")
+    public void receiveMessage(String message) {
+        log.info("Received " + message);
     }
 }
